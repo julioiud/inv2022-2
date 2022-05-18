590b018c78fd3e127bf65bb58140dfd1e91423b7
jmartinez <jmartinez@cobiscorp.com>
2022-05-18 05:38:44 +0200

    :sparkles: adding CRUD TipoEquipo

--- routes/tipoEquipo.js
+++ routes/tipoEquipo.js
@@ -1,53 +1,35 @@
 const { Router } = require('express');
 /*const { TipoEquipo } = require('../models/tipoEquipo');
 const { Usuario } = require('../models/usuario');*/
-const { getTiposEquipo } = require('../controllers/tipoEquipo');
+const { getTiposEquipo, getTiposEquipoUserActivo, createTipoEquipo, getTiposEquipoById, updateTipoEquipoById, deleteTipoEquipoByID } = require('../controllers/tipoEquipo');
 
 const router = Router();
+
+/**
+ * btiene todos los tipos de equipos los cuales los usuarios
+ * son activos
+ */
+router.get('/user-activo', getTiposEquipoUserActivo);
 
 /**
  * Obtiene todos los tipos de equipos
  */
 router.get('/', getTiposEquipo);
-    /*const tiposEquipo =  [
-        {
-            ....
-            usuario: adadadasdasdasd => true
-        },
-        {
-            ....
-            usuario: xxxxxxxxxxxxxxxd => false
-        },
-    ]   
-   const resultado =  tiposEquipo.filter(t => {
-        const queryUser = {_id: t.usuario}
-        const usuario = Usuario.findOne(queryUser);
-        if(usuario.estado){
-            return true;
-        }
-        return false;
-    })  */  
 
 /**
  * Obtiene un tipos de equipos por id
  */
- router.get('/:id', (req, res) => {
-    res.json({});
-});
+ router.get('/:id', getTiposEquipoById);
 
 /**
  * Crear un tipos de equipos
  */
-router.post('/', (req, res) => {
-    res.json({});
-});
+router.post('/', createTipoEquipo);
 
 /**
  * Actualiza un tipos de equipos por id
  */
-router.put('/:id', (req, res) => {
-    res.json({});
-});
+router.put('/:id', updateTipoEquipoById);
 
 /**
  * Actualiza una parte del tipos de equipos
@@ -59,8 +41,6 @@ router.patch('/:id', (req, res) => {
 /**
  * Borra un tipos de equipos por id
  */
- router.delete('/:id', (req, res) => {
-    res.json({});
-});
+ router.delete('/:id', deleteTipoEquipoByID);
 
 module.exports = router;
