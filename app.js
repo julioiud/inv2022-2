f63bed49477fdb3a3c0b5e8676f3ef22b6c33ed7
jmartinez <jmartinez@cobiscorp.com>
2022-06-02 05:57:00 +0200

    :egg: adding cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 
 const fileUpload = require('express-fileupload');
+const cors = require('cors');
 
 const usuarios = require('./routes/usuario');
 const tiposEquipo = require('./routes/tipoEquipo');
@@ -14,11 +15,12 @@ const inventarios = require('./routes/inventario');
 //middlewares
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
-
 app.use(fileUpload({
     useTempFiles : true,
     tempFileDir : '/tmp/'
 }));
+app.use(cors());
+
 
 app.use('/api/usuarios', usuarios);
 app.use('/api/tiposequipo', tiposEquipo);
